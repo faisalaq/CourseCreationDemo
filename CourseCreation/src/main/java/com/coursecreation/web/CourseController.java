62df3fd6c0052ae417e58a0afdeef2f718b35459
Faisal <faq01818@gmail.com>
2016-11-01 22:02:07 -0700

    commit 3 for CourseCreationApp added 1) CourseRepo interface 2) +editCourse.html 2) +createCourse.html

--- CourseCreation/src/main/java/com/coursecreation/web/CourseController.java
+++ CourseCreation/src/main/java/com/coursecreation/web/CourseController.java
@@ -1,10 +1,33 @@
 package com.coursecreation.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.coursecreation.domain.Course;
+import com.coursecreation.repository.CourseRepository;
 
 @Controller
 public class CourseController {
+
+	private CourseRepository courseRepo; 
+	
+	
+	
+	public CourseRepository getCourseRepo() {
+		return courseRepo;
+	}
+
+	@Autowired
+	public void setCourseRepo(CourseRepository courseRepo) {
+		this.courseRepo = courseRepo;
+	}
 
 	@RequestMapping("/")
 	public String root(){
@@ -13,9 +36,31 @@ public class CourseController {
 	}
 	
 	@RequestMapping("courses")
-	public String  courses(){
-		
+	public String  courses(ModelMap model){
+		List<Course> courses = courseRepo.findAll();
+		model.put("courses", courses);
 		return "courses";
 	}
+	
+	@RequestMapping(value="editCourse/{courseId}", method=RequestMethod.GET)
+	public String editCourse(@PathVariable Long courseId, ModelMap model){
+		
+		return "editCourse";
+	}
+	
+	@RequestMapping(value="createCourse", method=RequestMethod.GET)
+	public String createCourseGet(ModelMap model){
+		Course course = new Course();
+		model.put("course", course);
+		return "createCourse";
+	}
+	
+	@RequestMapping(value="createCourse", method=RequestMethod.POST)
+	public String createCoursePost(@ModelAttribute Course course, ModelMap model){
+		courseRepo.save(course);
+		return "redirect:/";
+	}
+	
+	
 	
 }
