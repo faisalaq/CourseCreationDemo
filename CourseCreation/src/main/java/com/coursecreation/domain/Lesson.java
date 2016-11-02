7f3a5e529607b94c8b97a4659e4df07b9cbc8c12
Faisal <faq01818@gmail.com>
2016-11-01 21:15:59 -0700

    commit 2 for CourseCreation, added 1)Created tables from domain objects  2) Created One to many relationship between Course and Lesson 3) Added a thyme leaf statement to courses.html

--- CourseCreation/src/main/java/com/coursecreation/domain/Lesson.java
+++ CourseCreation/src/main/java/com/coursecreation/domain/Lesson.java
@@ -1,20 +1,42 @@
 package com.coursecreation.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Lesson {
 
-	private int number;
-	private String name;
-	public int getNumber() {
-		return number;
+	private String title;
+	private int id;
+	private Course course;
+	
+	
+	public String getTitle() {
+		return title;
 	}
-	public void setNumber(int number) {
-		this.number = number;
+	
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	public String getName() {
-		return name;
+	
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
 	}
-	public void setName(String name) {
-		this.name = name;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@ManyToOne
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
 	}
 	
 	
